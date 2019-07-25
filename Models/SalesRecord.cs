4d638fdee28bfb71b7e58ac7144678167b3cc747
Daniel Felix <wwfianna@hotmail.com>
2019-07-24 21:23:24 -0300

    Seeding Service -- 233

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -13,11 +13,12 @@ namespace SalesWebMvc.Models {
 
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status) {
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller) {
             Id = id;
             Date = date;
             Amount = amount;
             Status = status;
+            Seller = seller;
         }
     }
 }
